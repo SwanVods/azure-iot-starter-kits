01dc458a793662ae62e85432c9702632d3cde5c6
Arief G <ariefgraifhan@gmail.com>
2022-06-22 03:45:57 +0700

    FIX KENEK

--- seeed/1-temperature-sensor/modules/temperature-sensor/main.py
+++ seeed/1-temperature-sensor/modules/temperature-sensor/main.py
@@ -5,8 +5,6 @@ import time
 import os
 import pandas as pd
 
-import azure.cosmos.cosmos_client as cosmos_client
-
 import cosmos_config as config
 
 from gpiozero.pins.native import NativeFactory
@@ -14,12 +12,12 @@ from gpiozero import Buzzer
 from azure.iot.device.aio import IoTHubDeviceClient
 from bme280sensor import BME280Sensor
 from azure.iot.device import Message
-import aioschedule as schedule
+from azure.cosmos.cosmos_client import CosmosClient
 
 BME280_SEND_ENABLED = True
 BME280_SEND_INTERVAL_SECONDS = 15
 
-CONNECTION_STRING = os.getenv("DEVICE_CS")
+CONNECTION_STRING = os.getenv("DEVICE_CS", "HostName=proyek-akhir.azure-devices.net;DeviceId=raspberry;SharedAccessKey=l92jykAxb6ey8Ux0TgravAZC9VAPszDtH+lYPpiR9OE=")
 
 HOST = config.settings['host']
 MASTER_KEY = config.settings['master_key']
@@ -32,15 +30,11 @@ def main():
     hub_client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
     
     loop = asyncio.get_event_loop()
-    
-    
-    
-    schedule.every(30).minutes.do(insert_to_database, message='things')
 
     try:
         loop.create_task(stream_sensor_data(hub_client, BME280Sensor(), Buzzer(12, pin_factory=NativeFactory())))
-        loop.create_task(schedule.run_pending())
-        # Run the sample in the event loop
+        loop.create_task(insert_to_database())
+        
         loop.run_forever()
         
     except KeyboardInterrupt:
@@ -49,35 +43,33 @@ def main():
         # Upon application exit, shut down the client
         print("Shutting down IoTHubClient")
         loop.run_until_complete(hub_client.shutdown())
-        loop.close()
+        loop.stop()
     
     
 async def insert_to_database():
     global df
-    
-    # Create the Cosmos client object
-    db_client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY})
-    db = db_client.get_database_client(DATABASE_ID)
-    print('Database with id \'{0}\' was found'.format(DATABASE_ID))
-    
-    container = db.get_container_client(CONTAINER_ID)
-    
-    temperature = df['temperature'].mean()
-    humidity = df['humidity'].mean()
-    pressure = df['pressure'].mean()
-    
-    data = {
-        'id' : str(int(time.time())),
-        'temperature_avg' : temperature,
-        'humidity_avg' : humidity,
-        'pressure_avg' : pressure
-    }
-    
-    container.create_item(body=data)
-    print('Inserted to database')
-    
-    # reset df
-    df = pd.DataFrame(columns=['temperature', 'humidity', 'pressure'])
+    while True:
+        await asyncio.sleep(1800)
+        # Create the Cosmos client object
+        db_client = CosmosClient(HOST, {'masterKey': MASTER_KEY})
+        
+        temperature = df['temperature'].mean()
+        humidity = df['humidity'].mean()
+        pressure = df['pressure'].mean()
+        
+        data = {
+            'id' : str(int(time.time())),
+            'temperature_avg' : temperature,
+            'humidity_avg' : humidity,
+            'pressure_avg' : pressure
+        }
+        
+        db_client.UpsertItem("dbs/" + DATABASE_ID + "/colls/" + CONTAINER_ID, data)
+        print('Inserted to database')
+        
+        # reset df
+        df = pd.DataFrame(columns=['temperature', 'humidity', 'pressure'])
+        
 
 async def record_sensor_data(bme280_sensor):
     temperature_sum = 0
@@ -114,7 +106,7 @@ async def stream_sensor_data(hub_client, bme280_sensor, buzzer):
             print("Avg. Temperature: {0:0.2f} C, \nAvg. Humidity: {1:0.2f} %, \nAvg. Pressure: {2:0.3f} hPa\n".format(temperature, humidity, pressure))
             
             if(temperature > 32) :
-                buzzer.beep(off_time=0.5, n=5)
+                buzzer.beep(off_time=0.25, n=5)
 
             message = Message(json.dumps({
                 "deviceId": "BME280",   
@@ -130,7 +122,6 @@ async def stream_sensor_data(hub_client, bme280_sensor, buzzer):
             message.content_type = "application/json"
             message.custom_properties["temperatureAlert"] = "true" if temperature > 32 else "false"
             await hub_client.send_message(message)
-        
 
 # run the script
 if __name__ == '__main__':
